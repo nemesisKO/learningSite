9e2ecb6737df7941d6e0eac98e3f65033a8e3925
kaveh <kaveh.zhian@gmail.com>
2019-07-30 16:40:06 +0430

    38

---
+++ resources/js/components/Login.vue
@@ -0,0 +1,131 @@
+ <template>
+  <div class="container">
+    <div
+      class="modal fade"
+      id="loginModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="card card-shadowed p-50 w-400 mb-0" style="max-width: 100%">
+          <h5 class="text-uppercase text-center">Login</h5>
+          <br />
+          <br />
+
+          <form>
+            <ul class="list-group alert alert-danger" v-if="errors.length>0">
+              <li
+                class="list-group-item"
+                v-for="error in errors"
+                :key="errors.indexOf(error)"
+              >{{error}}</li>
+            </ul>
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Email" v-model="email" />
+            </div>
+
+            <div class="form-group">
+              <input type="password" class="form-control" placeholder="Password" v-model="password" />
+            </div>
+
+            <div class="form-group flexbox py-10">
+              <label class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" checked v-model="remember" />
+                <span class="custom-control-indicator"></span>
+                <span class="custom-control-description">Remember me</span>
+              </label>
+
+              <a class="text-muted hover-primary fs-13" href="#">Forgot password?</a>
+            </div>
+
+            <div class="form-group">
+              <button
+                class="btn btn-bold btn-block btn-primary"
+                :disabled="!isValidLoginForm"
+                type="submit"
+                @click="attemptLogin()"
+              >Login</button>
+            </div>
+          </form>
+
+          <div class="divider">Or Sign In With</div>
+          <div class="text-center">
+            <a class="btn btn-circular btn-sm btn-facebook mr-4" href="#">
+              <i class="fa fa-facebook"></i>
+            </a>
+            <a class="btn btn-circular btn-sm btn-google mr-4" href="#">
+              <i class="fa fa-google"></i>
+            </a>
+            <a class="btn btn-circular btn-sm btn-twitter" href="#">
+              <i class="fa fa-twitter"></i>
+            </a>
+          </div>
+
+          <hr class="w-30" />
+
+          <p class="text-center text-muted fs-13 mt-20">
+            Don't have an account?
+            <a href="page-register.html">Sign up</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      remember: true,
+      loading: false,
+      errors: []
+    };
+  },
+  methods: {
+    validateEmail() {
+      {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)) {
+          return true;
+        }
+
+        return false;
+      }
+    },
+    attemptLogin() {
+      this.errors = [];
+      this.loading = true;
+      axios
+        .post("/login", {
+          email: this.email,
+          password: this.password,
+          remember: this.remember
+        })
+        .then(response => {
+          location.reload();
+        })
+        .catch(error => {
+          this.loading = false;
+          if (error.response.status == 422) {
+            this.errors.push("peida nashod in user");
+          } else {
+            this.errors.push("ie eshtebah pish oomade badan try kon");
+          }
+        });
+    }
+  },
+  computed: {
+    isValidLoginForm() {
+      return this.validateEmail() && this.password && !this.loading;
+    }
+  },
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
+</script>
